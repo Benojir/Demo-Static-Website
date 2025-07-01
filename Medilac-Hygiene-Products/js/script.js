eb4495076b4c87ff5fa6efb697dce7c1a9ea1e8e
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-07-01 23:59:27 +0530

    added upcoming section

--- Medilac-Hygiene-Products/js/script.js
+++ Medilac-Hygiene-Products/js/script.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Mobile Menu Toggle
-    $('.mobile-menu').click(function() {
+    $('.mobile-menu').click(function () {
         $('.nav').toggleClass('active');
         $(this).find('i').toggleClass('fa-times');
     });
 
     // Smooth Scrolling
-    $('a[href*="#"]').on('click', function(e) {
+    $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
-        
+
         $('html, body').animate(
             {
                 scrollTop: $($(this).attr('href')).offset().top - 80,
@@ -16,7 +16,7 @@ $(document).ready(function() {
             500,
             'linear'
         );
-        
+
         // Close mobile menu when clicking a link
         if ($('.nav').hasClass('active')) {
             $('.nav').removeClass('active');
@@ -25,7 +25,7 @@ $(document).ready(function() {
     });
 
     // Header Scroll Effect
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.header').addClass('scrolled');
         } else {
@@ -84,6 +84,46 @@ $(document).ready(function() {
         arrows: false,
     });
 
+    // Upcoming products slider
+    $('.upcoming-products-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.upcoming-products-slider').on('setPosition', function () {
+        var maxHeight = 0;
+        $('.upcoming-product-item').css('height', 'auto').each(function () {
+            if ($(this).height() > maxHeight) {
+                maxHeight = $(this).height();
+            }
+        });
+        $('.upcoming-product-item').height(maxHeight);
+    });
+
+
     // Initialize Fancybox
     Fancybox.bind("[data-fancybox]", {
         // Your custom options
@@ -91,11 +131,11 @@ $(document).ready(function() {
 
     // Animation on Scroll
     function animateOnScroll() {
-        $('.animate__animated').each(function() {
+        $('.animate__animated').each(function () {
             var position = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
-            
+
             if (scroll + windowHeight > position) {
                 var animation = $(this).data('animation');
                 $(this).addClass(animation);
@@ -105,14 +145,14 @@ $(document).ready(function() {
 
     // Run on page load
     animateOnScroll();
-    
+
     // Run on scroll
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         animateOnScroll();
     });
 
     // Form Submission
-    $('#enquiryForm').submit(function(e) {
+    $('#enquiryForm').submit(function (e) {
         e.preventDefault();
         // Get the form data and send it to mailto
         var formData = $(this).serialize();
